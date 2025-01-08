e25a4b468db0a703f7f8d108861013831dd19d5d
Paulin DOYON <paulin.doyon15@gmail.com>
2025-01-08 11:44:32 +0100

    ajout fonction calcul longueur aretes

--- fonctions.py
+++ fonctions.py
@@ -1,16 +1,17 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 from math import sqrt
 from random import random
 from typing import List, Dict, Tuple
 
-def lecture(fichier:str) -> Tuple[nx.DiGraph,Dict, List]:
+def lecture(fichier:str) -> Tuple[nx.Graph,Dict, List]:
     '''
     fonction qui lis les donnees d'un fichier et qui retourne le graphe, les positions et les alignements
     Args:
         fichier (str): emplacement fichier entree
     Returns:
-        Tuple[nx.DiGraph,dict, list]
+        Tuple[nx.Graph,dict, list]
     '''
 
     g = nx.Graph()
@@ -25,7 +26,7 @@ def lecture(fichier:str) -> Tuple[nx.DiGraph,Dict, List]:
 
         # placement des positions avec leur sommet dans un dico
         for point in b:
-            positions[point[0]] = (point[2], point[4])
+            positions[point[0]] = (int(point[2]), int(point[4]))
 
         # placement des sommets alignes
         for ligne in c.split("\n")[1:]:
@@ -39,9 +40,41 @@ def lecture(fichier:str) -> Tuple[nx.DiGraph,Dict, List]:
     aretes = [i.split(";") for i in c.split("\n")[1:]]
     aretes = [item for sublist in aretes for item in sublist]
     edges = [(i[0], i[2]) for i in aretes]
-    g:nx.DiGraph=nx.DiGraph()
+    g.add_edges_from(edges)
 
-    return (g.add_edges_from(edges),positions,alignements)
+    return (g,positions,alignements)
 
+def longueur_arete(Graph:nx.Graph,postion_sommet:Dict[str,Tuple[str,str]]) -> nx.Graph :
+    '''Cette fonction prend en entré un graph et la postion de ces sommets
+    et retourne un graphe avec pour chaque arête la longeur de celle si.
+        
+    Args:
+        Graphe (nx.Graph): Graphe des couloir
+        postion_sommet (Dict[str:Tuple[str,str]): Le mot de passe pour la connexion.
+        alignements (List[List[str,str]]): Liste contenant les alignement des points.
 
-Graph,positions,alignements = lecture("graphe.txt")
+    Returns:
+        nx.Graph: Graph avec les longeur des arete.
+        
+    '''
+    #On cree une liste d'arrete initialement vide
+    liste_arrete=[]
+
+    #Parcour de toute les aretes du Graph
+    for arete in Graph.edges():
+        #Recuperation des coordonnees X Y de chacune des extremite des aretes
+        pos_a=postion_sommet[arete[0]]
+        pos_b=postion_sommet[arete[1]]
+        #Calcule de la longueur
+        longueur=sqrt((pos_b[0]-pos_a[0])**2+(pos_b[1]-pos_a[1])**2)
+        #Ajout tu tuple (sommet_debut,sommet_fin,{'longeur':valeur_longeur}) dans la liste des aretes
+        liste_arrete.append((arete[0],arete[1],{'longeur':longueur}))
+    
+    #Création d'un graph vide
+    Graphe_final:nx.Graph=nx.Graph()
+    #Ajout des aretes et renvoie
+    Graphe_final.add_edges_from(liste_arrete)
+    return Graphe_final
+
+G,positions,alignements = lecture("graphe.txt")
+G=longueur_arete(G,positions)
